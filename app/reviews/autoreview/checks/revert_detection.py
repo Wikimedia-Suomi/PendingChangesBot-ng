f873d455d185444aac65f946538863f961f56d7e
AmbatI_Teja_Sri_Surya <ambatisurya1203@gmail.com>
2025-10-29 21:28:02 -0500

    style: sort imports per ruff (I001)

--- app/reviews/autoreview/checks/revert_detection.py
+++ app/reviews/autoreview/checks/revert_detection.py
@@ -11,8 +11,8 @@ from typing import Any
 
 from django.conf import settings
 
-from ..context import CheckContext
 from .. import SupersetQuery
+from ..context import CheckContext
 
 logger = logging.getLogger(__name__)
 
